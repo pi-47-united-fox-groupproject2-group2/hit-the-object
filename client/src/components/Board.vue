59dbe483f1768c3b4bf550acbf13e8f39e8edb68
Samuel Aditia <samueladitia95@gmail.com>
2020-10-16 06:44:00 +0700

    1. Fix timer and random move 2. Import Board.vue from Lounge

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1>Whack a mole!</h1>
-
     <h2>Your score:</h2>
     <h2 id="score">{{ score }}</h2>
 
@@ -16,8 +14,9 @@
         @click.prevent="answer(buttonNumber)"
       ></div>
     </div>
-
-    <button class="btn btn-lg btn-dark" @click="start()">Start</button>
+    <div class="start-button-div">
+      <button class="btn btn-lg btn-dark" @click="start()">Start</button>
+    </div>
   </div>
 </template>
 
@@ -27,7 +26,7 @@ export default {
     return {
       timer: 60,
       score: 0,
-      currentNumber: 0
+      currentNumber: 0,
     };
   },
   methods: {
@@ -38,19 +37,24 @@ export default {
       }
     },
     start() {
-      let time = setInterval(() => {
-        this.countDown();
-      }, 1000);
-      let randomPos = setInterval(() => {
-        this.setRandomNumber();
-      }, 500);
-      setTimeout(() => {
-        clearInterval(time);
-        clearInterval(randomPos);
-      }, 61000);
+      this.countDown();
+      this.randomMove(this.timer * 1000 + 1000);
     },
     countDown() {
-      this.timer--;
+      if (this.timer > 0) {
+        setTimeout(() => {
+          this.timer--;
+          this.countDown();
+        }, 1000);
+      }
+    },
+    randomMove(totalTime) {
+      let interval = setInterval(() => {
+        this.setRandomNumber();
+      }, 600);
+      setTimeout(() => {
+        clearInterval(interval);
+      }, totalTime);
     },
     setRandomNumber() {
       this.currentNumber = Math.floor(Math.random() * 9 + 1);
@@ -61,17 +65,22 @@ export default {
         classNames.push("mole");
       }
       return classNames;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
+.start-button-div {
+  display: flex;
+  justify-content: center;
+}
+
 .square {
   width: 200px;
   height: 200px;
   border-style: solid;
-  border-color: black;
+  border-color: white;
 }
 
 .grid {
@@ -84,7 +93,7 @@ export default {
 }
 
 .mole {
-  background-color: black;
-  background-size: cover;
+  background-image: url("../assets/mole.png");
+  background-size: 125% 100%;
 }
 </style>
--- client/src/views/Lounge.vue
+++ client/src/views/Lounge.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
-    <h1>Hi {{username}}, Your on Public Room</h1>
-
+    <h1>Hi {{ username }}, Your on Public Room</h1>
+    <div class="board-div">
+      <Board />
+    </div>
   </div>
 </template>
 
 <script>
+import Board from "@/components/Board.vue";
 export default {
   computed: {
-    username () {
-      return this.$store.state.username
-    }
-  }
-}
+    username() {
+      return this.$store.state.username;
+    },
+  },
+  components: {
+    Board,
+  },
+};
 </script>
 
 <style lang="scss">
 h1 {
-    text-align: center;
-    color: white;
+  text-align: center;
+  color: white;
+}
+.board-div {
+  display: flex;
+  justify-content: center;
+  color: white;
 }
 </style>
