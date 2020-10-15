5fee471c361847d16bbff9dbf11441ba4623d33b
midham14 <mohammadidham14@gmail.com>
2020-10-15 21:04:47 +0700

    1. add socket to server & client

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+      <input type="text" v-model="username">
+      <button @click.prevent="onSubmitUser">submit username</button>
+      <div class="container row">
+        <div class="col-4" 
+        v-for="(user,i) in dataUser"
+        :key="i"
+        >
+          <div>{{user.username}}</div>
+          <div>{{user.score}}</div>
+        </div>
+
+      </div>
+        <button class="btn btn-primary" @click.prevent="scoreAdd">add</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      username:'',
+      dataUser:[]
+    }
+  },
+  sockets:{
+    user_Connected: function (data) {
+      console.log(data,'<><><><>')
+    },
+    get_user: function(data) {
+      this.dataUser=data
+    }
+  },
+  methods:{
+    onSubmitUser () {
+      localStorage.setItem('username',this.username)
+      this.$socket.emit('emit_new_user',{username:this.username,score:0})
+    },
+    scoreAdd () {
+    this.$socket.emit('add_score',{username:localStorage.username})
+  }
   }
 }
 </script>
--- server/app.js
+++ server/app.js
@@ -3,12 +3,30 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 
+let listUser = []
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
   });
 
 io.on('connection', (socket) => {
 console.log('a user connected');
+  io.emit('user_Connected',{message:'welcome to socket'})
+
+  // tangkep new user
+  socket.on('emit_new_user', (data)=>{
+    listUser.push(data)
+    io.emit('get_user', listUser)
+  })
+
+  socket.on('add_score', (data)=>{
+    listUser.forEach(val=>{
+      if(val.username === data.username){
+        val.score++
+      }
+    })
+    io.emit('get_user', listUser)
+  })
 });
 
   
