80c707160daf1c767970e33ebfe3236dc870391d
Samuel Aditia <samueladitia95@gmail.com>
2020-10-16 12:46:28 +0700

    Server ready deploy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
 
 let listUser = []
@@ -50,6 +51,6 @@ io.on('connection', (socket) => {
 });
 
   
-http.listen(3000, () => {
-console.log('listening on *:3000');
+http.listen(port, () => {
+console.log(`listening on *:${port}`);
 });
